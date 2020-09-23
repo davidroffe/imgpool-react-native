644bc2e61ee0692f8249a3aa695bdfb97d91d557
David Roffe <davidroffe@gmail.com>
2020-09-22 22:16:52 -0400

    Initial form of PostDetail component

--- src/screens/PostDetail.js
+++ src/screens/PostDetail.js
@@ -1,8 +1,29 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { View, Image, StyleSheet } from 'react-native';
 import { connect } from 'react-redux';
-export const PostDetail = ({ navigation, route }) => {
-  return <Text>Post Detail</Text>;
+import * as actions from '../actions';
+import { API_URL } from '@env';
+
+export const PostDetail = (props) => {
+  const aspectRatio =
+    props.route.params.post.height / props.route.params.post.width > 1
+      ? props.route.params.post.height / props.route.params.post.width
+      : props.route.params.post.width / props.route.params.post.height;
+
+  return (
+    <View>
+      <Image
+        style={{ ...styles.thumbnail, aspectRatio }}
+        source={{ uri: `${API_URL}${props.route.params.post.url}` }}
+      />
+    </View>
+  );
 };
 
-export default connect(null)(PostDetail);
+export default connect(null, actions)(PostDetail);
+
+const styles = StyleSheet.create({
+  thumbnail: {
+    width: '100%',
+  },
+});
