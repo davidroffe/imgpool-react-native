b38ef1dd59b17a25a3441a34e13e7909012830b0
David Roffe <davidroffe@gmail.com>
2020-10-10 21:15:15 -0400

    BorderButton to have dark/light version

--- src/components/BorderButton.js
+++ src/components/BorderButton.js
@@ -1,22 +1,33 @@
 import React from 'react';
 import { TouchableHighlight, View, Text, StyleSheet } from 'react-native';
 
-const BorderButton = ({ text, onPress }) => {
+const BorderButton = ({ text, onPress, shade }) => {
   return (
     <TouchableHighlight
       activeOpacity={0.5}
       underlayColor={'white'}
       onPress={onPress}
     >
-      <View style={styles.button}>
-        <Text style={styles.text}>{text}</Text>
+      <View style={shade === 'dark' ? styles.darkButton : styles.lightButton}>
+        <Text style={shade === 'dark' ? styles.darkText : styles.lightText}>
+          {text}
+        </Text>
       </View>
     </TouchableHighlight>
   );
 };
 
 const styles = StyleSheet.create({
-  button: {
+  darkButton: {
+    height: 50,
+    width: 326,
+    maxWidth: '100%',
+    borderColor: '#333',
+    borderWidth: 2,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  lightButton: {
     height: 50,
     width: 326,
     maxWidth: '100%',
@@ -25,7 +36,12 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
-  text: {
+  darkText: {
+    fontWeight: '800',
+    fontSize: 12,
+    color: '#333',
+  },
+  lightText: {
     fontWeight: '800',
     fontSize: 12,
     color: '#fffef2',
