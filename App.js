136802c41373ef969ccc3df0d0881a5f9e4ab726
David Roffe <davidroffe@gmail.com>
2020-10-11 08:43:21 -0400

    Using StackNavigator for Account screens within TabNavigator

--- App.js
+++ App.js
@@ -11,6 +11,7 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import PostList from './src/screens/PostList';
 import PostDetail from './src/screens/PostDetail';
@@ -20,7 +21,16 @@ import MainMenu from './src/components/MainMenu';
 import reducers from './src/reducers';
 
 const Tab = createBottomTabNavigator();
+const AccountStack = createStackNavigator();
 const store = createStore(reducers, applyMiddleware(thunk));
+
+const AccountStackScreen = () => {
+  return (
+    <AccountStack.Navigator>
+      <AccountStack.Screen name="Account" component={Account} />
+    </AccountStack.Navigator>
+  );
+};
 
 const App: () => React$Node = () => {
   return (
@@ -34,7 +44,7 @@ const App: () => React$Node = () => {
         >
           <Tab.Screen name="PostList" component={PostList} />
           <Tab.Screen name="PostDetail" component={PostDetail} />
-          <Tab.Screen name="Account" component={Account} />
+          <Tab.Screen name="Account" component={AccountStackScreen} />
           <Tab.Screen name="Login" component={Login} />
         </Tab.Navigator>
       </NavigationContainer>
